ce7c39e4764ac03e73b9147467a524194177f53d
rameshsoft9177791456 <rameshsoft.9177791456@gmail.com>
2022-09-18 19:14:23 +0530

    stash message

--- src/com/rameshsoft/java/gitdemos/HardworkJobTest.java
+++ src/com/rameshsoft/java/gitdemos/HardworkJobTest.java
@@ -3,5 +3,9 @@ package com.rameshsoft.java.gitdemos;
 public class HardworkJobTest {
 public static void main(String[] args) {
 	System.out.println("PRACTICE = JOB");
+	System.out.println("TS1");
+	System.out.println("TS2");
+	System.out.println("TS3");
+	System.out.println("TS4");
 }
 }
--- src/com/rameshsoft/java/gitdemos/StashHello2.java
+++ src/com/rameshsoft/java/gitdemos/StashHello2.java
@@ -4,6 +4,7 @@ public class StashHello2 {
 	public static void main(String[] args) {
 		
 		System.out.println("9177791456");
-		
+		System.out.println("91456");
+		System.out.println("Hello 456");
 	}
 }
